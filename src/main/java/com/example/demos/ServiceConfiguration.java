13211cbe78d7b62d236f8fba1f858808c859e398
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-18 16:22:28 -0300

    Beans

---
+++ src/main/java/com/example/demos/ServiceConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.demos;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfiguration {
+
+    @Bean
+    public InventoryService inventoryService(){
+        return new InventoryService();
+    }
+
+    @Bean
+    public ProductService productService(){
+        return new ProductService(inventoryService());
+    }
+}
