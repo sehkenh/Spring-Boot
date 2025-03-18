6f84e79819f661dd2dfaf6259ad739d0f2ea6f76
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-03-18 17:22:25 -0300

    Beans Configuration

--- src/main/java/com/example/demos/ServiceConfiguration.java
+++ src/main/java/com/example/demos/ServiceConfiguration.java
@@ -3,7 +3,6 @@ package com.example.demos;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class ServiceConfiguration {
 
